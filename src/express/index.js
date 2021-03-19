8d903b3e1440cac127522765ff686a97a7624b76
evgeniy <not-found@inbox.ru>
2021-03-19 16:37:05 +0300

    Добавлены статические файлы, реализовано их подключение в express; добавлены pug-шаблоны страниц с ошибками

--- src/express/index.js
+++ src/express/index.js
@@ -1,17 +1,24 @@
 "use strict";
 
 const express = require(`express`);
+const path = require(`path`);
 
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const offersRoutes = require(`./routes/offers-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 const app = express();
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.listen(DEFAULT_PORT, () => {
   console.log(`Слушаем порт ${DEFAULT_PORT}`);
